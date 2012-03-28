cc9979c67a81bb0202ea21ddb5b752a4de3cbce4
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2012-03-28 11:25:32 +0900

    Use edge evernote-mode
    Signed-off-by: Shin-ichiro OGAWA <rust.stnard+git@gmail.com>

--- .emacs.d/init.d/init_evernote.el
+++ .emacs.d/init.d/init_evernote.el
@@ -6,12 +6,16 @@
 ;;   Author  : Shin-ichiro OGAWA <rust@stnard.jp>
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'evernote-mode)
-(global-set-key "\C-cec" 'evernote-create-note)
-(global-set-key "\C-ceo" 'evernote-open-note)
-(global-set-key "\C-ces" 'evernote-search-notes)
-(global-set-key "\C-ceS" 'evernote-do-saved-search)
-(global-set-key "\C-cew" 'evernote-write-note)
+(when (require 'evernote-mode nil t)
+  (setq evernote-ruby-command "/usr/bin/ruby1.8")
+  (setq evernote-enml-formatter-command '("w3m" "-dump" "-I" "UTF8" "-O" "UTF8"))
+  (global-set-key "\C-cec" 'evernote-create-note)
+  (global-set-key "\C-ceo" 'evernote-open-note)
+  (global-set-key "\C-ces" 'evernote-search-notes)
+  (global-set-key "\C-ceS" 'evernote-do-saved-search)
+  (global-set-key "\C-cew" 'evernote-write-note)
+  (global-set-key "\C-cep" 'evernote-post-region)
+  (global-set-key "\C-ceb" 'evernote-browser))
 
 (provide 'init_evernote)
 ;; init_evernote.el ends here
