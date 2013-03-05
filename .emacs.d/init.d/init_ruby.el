ce5c699f477b499b20b26f9e6135771df3caac6e
Shin-ichiro OGAWA <shinichiro-ogawa@cookpad.com>
2013-03-05 11:54:05 +0900

    Add indentation patch to coding style

--- .emacs.d/init.d/init_ruby.el
+++ .emacs.d/init.d/init_ruby.el
@@ -60,5 +60,21 @@
           (lambda ()
             (define-key ruby-mode-map (kbd "C-'") 'anything-rdefs)))
 
+(setq ruby-deep-indent-paren-style nil)
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  (let ((column (current-column))
+        indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+        (setq offset (- column (current-column)))
+        (when (and (eq (char-after) ?\))
+                   (not (zerop (car state))))
+          (goto-char (cadr state))
+          (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
+
 (provide 'init_ruby)
 ;; init_ruby.el ends here
