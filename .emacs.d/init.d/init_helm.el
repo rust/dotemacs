6cb2a5810bd6c063bd67ebedfc33326cbed1a2fb
Shin-ichiro OGAWA <shinichiro-ogawa@cookpad.com>
2013-03-12 15:49:23 +0900

    Use helm for file-files

--- .emacs.d/init.d/init_helm.el
+++ .emacs.d/init.d/init_helm.el
@@ -9,8 +9,13 @@
 ;; helm
 (require 'helm-config)
 (require 'helm-command)
+
 (global-set-key "\C-xb" 'helm-mini)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+
 (helm-mode 1)
+;; (helm-dired-bindings 1)
 
 (provide 'init_helm)
 ;; init_helm.el ends here
