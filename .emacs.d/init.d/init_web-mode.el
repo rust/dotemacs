cf78aaaea9363559a2ca203d7486223d0cacfee9
Shin-ichiro OGAWA <shinichiro-ogawa@cookpad.com>
2013-03-13 09:50:54 +0900

    Add web-mode

---
+++ .emacs.d/init.d/init_web-mode.el
@@ -0,0 +1,30 @@
+;;; -*- mode: emacs-lisp; coding: utf-8; indent-tabs-mode: nil -*-
+
+;; init_web-mode.el
+
+;; Copyright (C) 2013 Shin-ichiro OGAWA
+;;   Author  : Shin-ichiro OGAWA <rust@stnard.jp>
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; web-mode
+(require 'web-mode)
+
+(add-to-list 'auto-mode-alist '("\\.phtml$"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp$"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x$"   . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
+
+(defun web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-html-offset   2)
+  (setq web-mode-css-offset    2)
+  (setq web-mode-script-offset 2)
+  (setq web-mode-php-offset    2)
+  (setq web-mode-java-offset   2)
+  (setq web-mode-asp-offset    2))
+(add-hook 'web-mode-hook 'web-mode-hook)
+
+(provide 'init_web-mode)
+;; init_web-mode.el ends here
