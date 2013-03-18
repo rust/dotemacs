fac99e2761a9646bfaaf7668c4662c5c135f67ff
Shin-ichiro OGAWA <shinichiro-ogawa@cookpad.com>
2013-03-18 09:20:17 +0900

    Use helm for yasnippet
    Signed-off-by: Shin-ichiro OGAWA <shinichiro-ogawa@cookpad.com>

--- .emacs.d/init.d/init_yasnippet.el
+++ .emacs.d/init.d/init_yasnippet.el
@@ -25,5 +25,27 @@
 ;; 既存スニペットを閲覧・編集する
 (define-key yas-minor-mode-map (kbd "C-x s v") 'yas-visit-snippet-file)
 
+;; helm interface
+(eval-after-load "helm-config"
+  '(progn
+     (defun my-yas/prompt (prompt choices &optional display-fn)
+       (let* ((names (loop for choice in choices
+                           collect (or (and display-fn (funcall display-fn choice))
+                                       choice)))
+              (selected (helm-other-buffer
+                         `(((name . ,(format "%s" prompt))
+                            (candidates . names)
+                            (action . (("Insert snippet" . (lambda (arg) arg))))))
+                         "*helm yas/prompt*")))
+         (if selected
+             (let ((n (position selected names :test 'equal)))
+               (nth n choices))
+           (signal 'quit "user quit!"))))
+     (custom-set-variables '(yas/prompt-functions '(my-yas/prompt)))
+     (define-key helm-command-map (kbd "y") 'yas/insert-snippet)))
+
+;; snippet-mode for *.yasnippet files
+(add-to-list 'auto-mode-alist '("\\.yasnippet$" . snippet-mode))
+
 (provide 'init_yasnippet)
 ;; init_yasnippet.el ends here
