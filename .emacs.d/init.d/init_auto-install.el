a69a67bffd558be3246a556daa26108081a2b8e8
Shinichiro OGAWA <shinichiro-ogawa@cookpad.com>
2013-09-30 09:39:15 +0900

    Remove unused elisp files

--- .emacs.d/init.d/init_auto-install.el
+++ .emacs.d/init.d/init_auto-install.el
@@ -2,18 +2,14 @@
 
 ;; init_auto-install.el
 
-;; Copyright (C) 2010 Shin-ichiro OGAWA
+;; Copyright (C) 2013 Shin-ichiro OGAWA
 ;;   Author  : Shin-ichiro OGAWA <rust@stnard.jp>
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; install-elips
-(require 'install-elisp)
-(setq install-elisp-repository-directory "~/.emacs.d/")
 ;; auto-install
 (require 'auto-install)
 (setq auto-install-directory "~/.emacs.d/auto-install/")
-;; (auto-install-update-emacswiki-package-name t)
-(auto-install-compatibility-setup)             ; 互換性確保
+(auto-install-update-emacswiki-package-name t)
 
 (provide 'init_auto-install)
 ;; init_auto-install.el ends here
