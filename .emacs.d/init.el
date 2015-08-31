0af906f5d8d10f85a6e63857b40ebfcfa98a033b
Shin-ichiro OGAWA <shinichiro-ogawa@cookpad.com>
2015-08-31 12:57:40 +0900

    Remove git-modes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,6 +7,13 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; path-list を load-path へ追加する
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (defun add-to-load-path (path-list)
   "Add paths to 'load-path"
   (let (path)
