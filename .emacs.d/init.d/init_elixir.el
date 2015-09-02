fb7d933f41635b1d33e43079c0547577bb92d8cc
Shin-ichiro OGAWA <shinichiro-ogawa@cookpad.com>
2015-09-02 11:49:02 +0900

    Remove dependency for ruby-end

--- .emacs.d/init.d/init_elixir.el
+++ .emacs.d/init.d/init_elixir.el
@@ -15,13 +15,6 @@
 
 (add-hook 'elixir-mode-hook 'ac-alchemist-setup)
 
-(add-to-list 'elixir-mode-hook
-             (defun auto-activate-ruby-end-mode-for-elixir-mode ()
-               (set (make-variable-buffer-local 'ruby-end-expand-keywords-before-re)
-                    "\\(?:^\\|\\s-+\\)\\(?:do\\)")
-               (set (make-variable-buffer-local 'ruby-end-check-statement-modifiers) nil)
-               (ruby-end-mode +1)))
-
 (sp-with-modes '(elixir-mode)
   (sp-local-pair "fn" "end"
          :when '(("SPC" "RET"))
