f00f67d320bd5f399a6c7048a72cef5687e142eb
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2015-09-05 01:36:52 +0900

    Update elixir smartparens setting

--- .emacs.d/init.d/init_elixir.el
+++ .emacs.d/init.d/init_elixir.el
@@ -15,14 +15,23 @@
 
 (add-hook 'elixir-mode-hook 'ac-alchemist-setup)
 
+(defun my-elixir-do-end-close-action (id action context)
+  (when (eq action 'insert)
+    (newline-and-indent)
+    (forward-line -1)
+    (indent-according-to-mode)))
+
 (sp-with-modes '(elixir-mode)
-  (sp-local-pair "fn" "end"
-         :when '(("SPC" "RET"))
-         :actions '(insert navigate))
+  (sp-local-pair "->" "end"
+                 :when '(("RET"))
+                 :post-handlers '(:add my-elixir-do-end-close-action)
+                 :actions '(insert)))
+
+(sp-with-modes '(elixir-mode)
   (sp-local-pair "do" "end"
-         :when '(("SPC" "RET"))
-         :post-handlers '(sp-ruby-def-post-handler)
-         :actions '(insert navigate)))
+                 :when '(("SPC" "RET"))
+                 :post-handlers '(:add my-elixir-do-end-close-action)
+                 :actions '(insert)))
 
 (provide 'init_elixir)
 ;; init_elixir.el ends here
