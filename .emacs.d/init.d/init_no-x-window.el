09a6855c52d3beea63209292273100eb90422fb3
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2016-03-08 10:21:52 +0900

    Fix terminal env setting
    Signed-off-by: Shin-ichiro OGAWA <rust.stnard+git@gmail.com>

--- .emacs.d/init.d/init_no-x-window.el
+++ .emacs.d/init.d/init_no-x-window.el
@@ -11,21 +11,7 @@
 ;; メニューバーを消す
 (menu-bar-mode -1)
 
-(load-theme 'solarized-dark)
-
-;; wb-line-number
-(require 'wb-line-number)
-;; (wb-line-number-toggle)
-;;(set-scroll-bar-mode nil)
-(setq wb-line-number-scroll-bar t)
-
-;; ddssk
 (global-set-key (kbd "C-m") 'newline-and-indent)
-(setq defalt-input-method "japanese-skk")
-(setq skk-henkan-strict-okuri-precedence t)
-(setq skk-check-okurigana-on-touroku t)
-(setq skk-server-host "localhost")
-(setq skk-server-portnum 1178)
 
 (provide 'init_no-x-window)
 ;; init_no-x-window.el ends here
