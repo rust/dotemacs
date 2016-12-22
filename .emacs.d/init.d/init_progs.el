3d7bd47230b3324fb66b4224a485a690a6142468
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2016-12-22 19:14:22 +0900

    Add php-mode
    Signed-off-by: Shin-ichiro OGAWA <rust.stnard+git@gmail.com>

--- .emacs.d/init.d/init_progs.el
+++ .emacs.d/init.d/init_progs.el
@@ -84,5 +84,20 @@
      (get-buffer-create "*scheme*"))
     (run-scheme scheme-program-name)))
 
+;; PHP
+(use-package php-mode
+  :config
+  (require 'cl)
+  (add-hook 'php-mode-hook
+            '(lambda ()
+               (auto-complete-mode t)
+               (require 'ac-php)
+               (setq ac-sources  '(ac-source-php ) )
+               (yas-global-mode 1)
+               (define-key php-mode-map  (kbd "C-]") 'ac-php-find-symbol-at-point)   ;goto define
+               (define-key php-mode-map  (kbd "C-t") 'ac-php-location-stack-back   ) ;go back
+               ))
+  )
+
 (provide 'init_progs)
 ;; init_progs.el ends here
