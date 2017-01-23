0d59915d7677d5c997dedbdcac3c0c57672a410d
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2017-01-23 17:29:10 +0900

    Fix macOS bifurcation

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -29,10 +29,9 @@
 (defvar x-window-p (and (eq window-system 'x) linux-p))
 (defvar no-x-p (and (not (eq window-system 'x)) linux-p))
 (defvar lubuntu-p (string= system-name "netbook"))
-(defvar macbook-air-p (string-match "Macbook-Air" "Shin-ichiro-no-MacBook-Air.local"))
 (defvar fullhd-p (string-match "fullhd" system-name))
 (defvar windows-p (equal window-system 'w32))
-(defvar sierra-p (string= system-name "MacBookPro.local"))
+(defvar sierra-p (string-match "16.3." (shell-command-to-string "uname -r")))
 
 ;; PATHの設定 from http://sakito.jp/emacs/emacsshell.html#path
 ;; より下に記述した物が PATH の先頭に追加されます
