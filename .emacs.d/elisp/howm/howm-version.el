ec4574df5691d52b64be7dcb0a61eed6d4b1805c
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2017-05-19 14:35:12 +0900

    Remove howm
    Signed-off-by: Shin-ichiro OGAWA <rust.stnard+git@gmail.com>

--- .emacs.d/elisp/howm/howm-version.el
+++
@@ -1,8 +0,0 @@
-;;; howm-version.el --- Wiki-like keyword jump between files
-;;; -*- Emacs-Lisp -*-
-;;; @configure_input@
-
-(provide 'howm-version)
-(defconst howm-version "InsertVersionHere")
-
-;;; howm-version.el ends here
