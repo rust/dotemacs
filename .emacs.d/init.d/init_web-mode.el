3d1ddbe9b5a491726369deb7b24f56103aa2007e
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2017-06-06 09:24:24 +0900

    Add eex files to web-mode

--- .emacs.d/init.d/init_web-mode.el
+++ .emacs.d/init.d/init_web-mode.el
@@ -15,6 +15,7 @@
 (add-to-list 'auto-mode-alist '("\\.as[cp]x$"   . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.eex$"       . web-mode))
 
 (defun web-mode-hook ()
   "Hooks for Web mode."
@@ -25,6 +26,13 @@
   (setq web-mode-java-offset   2)
   (setq web-mode-asp-offset    2))
 (add-hook 'web-mode-hook 'web-mode-hook)
+
+(setq web-mode-engines-alist
+      '(("ruby" . "\\.erb\\'")
+        ("elixir". "\\.eex\\'"))
+      )
+
+(setq web-mode-markup-indent-offset 2)
 
 (provide 'init_web-mode)
 ;; init_web-mode.el ends here
