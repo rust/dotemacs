e82e4d5fe5418a4065ac53959ef3ae6aa2ec143d
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2017-09-27 12:49:15 +0900

    Set web-mode to blade template file

--- .emacs.d/init.d/init_web-mode.el
+++ .emacs.d/init.d/init_web-mode.el
@@ -16,6 +16,7 @@
 (add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
 (add-to-list 'auto-mode-alist '("\\.eex$"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.blade\\."   . web-mode))
 
 (defun web-mode-hook ()
   "Hooks for Web mode."
