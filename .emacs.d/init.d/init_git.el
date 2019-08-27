c8360d0b3411840f213f66210d126c3375520f2a
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2019-08-27 18:12:09 +0900

    Use `use-package`

--- .emacs.d/init.d/init_git.el
+++ .emacs.d/init.d/init_git.el
@@ -11,22 +11,22 @@
 ;; magit.el
 (setq magit-last-seen-setup-instructions "1.4.0")
 
-(require 'magit)
-;;(require 'git-blame)
-
-;;(require 'git-gutter-fringe)
-
-(add-hook 'ruby-mode-hook 'git-gutter-mode)
-(add-hook 'enh-ruby-mode-hook 'git-gutter-mode)
-(add-hook 'coffee-mode-hook 'git-gutter-mode)
-(add-hook 'haml-mode-hook 'git-gutter-mode)
-(add-hook 'rhtml-mode-hook 'git-gutter-mode)
-(add-hook 'sass-mode-hook 'git-gutter-mode)
-(add-hook 'scss-mode-hook 'git-gutter-mode)
+(use-package magit
+  :after (ivy)
+  :config
+  (setq magit-completing-read-function 'ivy-completing-read))
 
 (use-package git-gutter+
   :ensure t
-  :init (global-git-gutter+-mode))
+  :init (global-git-gutter+-mode)
+  :config
+  (add-hook 'ruby-mode-hook 'git-gutter-mode)
+  (add-hook 'enh-ruby-mode-hook 'git-gutter-mode)
+  (add-hook 'coffee-mode-hook 'git-gutter-mode)
+  (add-hook 'haml-mode-hook 'git-gutter-mode)
+  (add-hook 'rhtml-mode-hook 'git-gutter-mode)
+  (add-hook 'sass-mode-hook 'git-gutter-mode)
+  (add-hook 'scss-mode-hook 'git-gutter-mode))
 
 (provide 'init_git)
 ;; init_git.el ends here
