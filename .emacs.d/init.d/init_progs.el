c38c7db5da40e7514a85ecd61a4b3a6f5b703990
Shin-ichiro OGAWA <rust.stnard@gmail.com>
2022-05-02 17:42:03 +0900

    Fix php-mode settings

--- .emacs.d/init.d/init_progs.el
+++ .emacs.d/init.d/init_progs.el
@@ -91,18 +91,9 @@
 
 ;; PHP
 (use-package php-mode
-  :config
-  (require 'cl)
-  (add-hook 'php-mode-hook
-            '(lambda ()
-               (auto-complete-mode t)
-               (require 'ac-php)
-               (setq ac-sources  '(ac-source-php ) )
-               (yas-global-mode 1)
-               (define-key php-mode-map  (kbd "C-]") 'ac-php-find-symbol-at-point)   ;goto define
-               (define-key php-mode-map  (kbd "C-t") 'ac-php-location-stack-back   ) ;go back
-               ))
-  )
+  :ensure t
+  :mode
+  ("\\.php" . pho-mode))
 
 ;; Rust
 (use-package rust-mode
