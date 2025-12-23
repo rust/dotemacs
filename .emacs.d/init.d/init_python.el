4a2578096a363dbca83f14d9669b77a18e1fc894
Shinichiro OGAWA <rust.stnard@gmail.com>
2025-12-23 12:01:19 +0900

    Remove autoload

--- .emacs.d/init.d/init_python.el
+++ .emacs.d/init.d/init_python.el
@@ -8,18 +8,12 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; python
 ;; pymacs
-(autoload 'pymacs-apply "pymacs")
-(autoload 'pymacs-call "pymacs")
-(autoload 'pymacs-eval "pymacs" nil t)
-(autoload 'pymacs-exec "pymacs" nil t)
-(autoload 'pymacs-load "pymacs" nil t)
 (eval-after-load "pymacs"
   '(add-to-list 'pymacs-load-path "~/.emacs.d/scripts"))
 ;; python-mode, pycomplete
 (setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
 (setq interpreter-mode-alist (cons '("python" . python-mode)
                                    interpreter-mode-alist))
-(autoload 'python-mode "python-mode" "Python editing mode." t)
 (add-hook 'python-mode-hook '(lambda ()
                                (require 'pycomplete)
                                ))
